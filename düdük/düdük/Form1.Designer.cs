9a8c624a91d1511d806df6fb12ddce9825688418
emirkerman07 <emircankerman27@gmail.com>
2025-03-05 13:45:38 +0300

    Add files via upload

---
+++ düdük/düdük/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace düdük
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.baslat = new System.Windows.Forms.Button();
+            this.durdur = new System.Windows.Forms.Button();
+            this.sifirla = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.kaydet = new System.Windows.Forms.Button();
+            this.progressBarHiz = new System.Windows.Forms.ProgressBar();
+            this.progressBarKilometre = new System.Windows.Forms.ProgressBar();
+            this.progressBarPil = new System.Windows.Forms.ProgressBar();
+            this.progressBarPilSicakligi = new System.Windows.Forms.ProgressBar();
+            this.labelHiz = new System.Windows.Forms.Label();
+            this.labelKilometre = new System.Windows.Forms.Label();
+            this.labelPil = new System.Windows.Forms.Label();
+            this.labelPilSicakligi = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // baslat
+            // 
+            resources.ApplyResources(this.baslat, "baslat");
+            this.baslat.BackColor = System.Drawing.Color.LightGreen;
+            this.baslat.Name = "baslat";
+            this.baslat.UseVisualStyleBackColor = false;
+            this.baslat.Click += new System.EventHandler(this.baslat_Click);
+            // 
+            // durdur
+            // 
+            resources.ApplyResources(this.durdur, "durdur");
+            this.durdur.BackColor = System.Drawing.Color.Salmon;
+            this.durdur.Name = "durdur";
+            this.durdur.UseVisualStyleBackColor = false;
+            this.durdur.Click += new System.EventHandler(this.durdur_Click);
+            // 
+            // sifirla
+            // 
+            resources.ApplyResources(this.sifirla, "sifirla");
+            this.sifirla.BackColor = System.Drawing.Color.LightCoral;
+            this.sifirla.Name = "sifirla";
+            this.sifirla.UseVisualStyleBackColor = false;
+            this.sifirla.Click += new System.EventHandler(this.sifirla_Click);
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // comboBox1
+            // 
+            resources.ApplyResources(this.comboBox1, "comboBox1");
+            this.comboBox1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Name = "comboBox1";
+            // 
+            // kaydet
+            // 
+            resources.ApplyResources(this.kaydet, "kaydet");
+            this.kaydet.BackColor = System.Drawing.Color.LightBlue;
+            this.kaydet.Name = "kaydet";
+            this.kaydet.UseVisualStyleBackColor = false;
+            this.kaydet.Click += new System.EventHandler(this.kaydet_Click);
+            // 
+            // progressBarHiz
+            // 
+            resources.ApplyResources(this.progressBarHiz, "progressBarHiz");
+            this.progressBarHiz.Name = "progressBarHiz";
+            // 
+            // progressBarKilometre
+            // 
+            resources.ApplyResources(this.progressBarKilometre, "progressBarKilometre");
+            this.progressBarKilometre.Name = "progressBarKilometre";
+            // 
+            // progressBarPil
+            // 
+            resources.ApplyResources(this.progressBarPil, "progressBarPil");
+            this.progressBarPil.Name = "progressBarPil";
+            // 
+            // progressBarPilSicakligi
+            // 
+            resources.ApplyResources(this.progressBarPilSicakligi, "progressBarPilSicakligi");
+            this.progressBarPilSicakligi.Name = "progressBarPilSicakligi";
+            // 
+            // labelHiz
+            // 
+            resources.ApplyResources(this.labelHiz, "labelHiz");
+            this.labelHiz.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.labelHiz.Name = "labelHiz";
+            // 
+            // labelKilometre
+            // 
+            resources.ApplyResources(this.labelKilometre, "labelKilometre");
+            this.labelKilometre.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.labelKilometre.Name = "labelKilometre";
+            // 
+            // labelPil
+            // 
+            resources.ApplyResources(this.labelPil, "labelPil");
+            this.labelPil.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelPil.Name = "labelPil";
+            // 
+            // labelPilSicakligi
+            // 
+            resources.ApplyResources(this.labelPilSicakligi, "labelPilSicakligi");
+            this.labelPilSicakligi.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelPilSicakligi.Name = "labelPilSicakligi";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Name = "label1";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(39)))));
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelPilSicakligi);
+            this.Controls.Add(this.labelPil);
+            this.Controls.Add(this.labelKilometre);
+            this.Controls.Add(this.labelHiz);
+            this.Controls.Add(this.progressBarPilSicakligi);
+            this.Controls.Add(this.progressBarPil);
+            this.Controls.Add(this.progressBarKilometre);
+            this.Controls.Add(this.progressBarHiz);
+            this.Controls.Add(this.kaydet);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.sifirla);
+            this.Controls.Add(this.durdur);
+            this.Controls.Add(this.baslat);
+            this.Name = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button baslat;
+        private System.Windows.Forms.Button durdur;
+        private System.Windows.Forms.Button sifirla;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button kaydet;
+        private System.Windows.Forms.ProgressBar progressBarHiz;
+        private System.Windows.Forms.ProgressBar progressBarKilometre;
+        private System.Windows.Forms.ProgressBar progressBarPil;
+        private System.Windows.Forms.ProgressBar progressBarPilSicakligi;
+        private System.Windows.Forms.Label labelHiz;
+        private System.Windows.Forms.Label labelKilometre;
+        private System.Windows.Forms.Label labelPil;
+        private System.Windows.Forms.Label labelPilSicakligi;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ düdük/düdük/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Windows.Forms;
+using System.IO;
+using System.Text;
+
+namespace düdük
+{
+    public partial class Form1 : Form
+    {
+        private string data;
+        private Queue<string> dataQueue = new Queue<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.ReadOnly = true;
+            string[] ports = SerialPort.GetPortNames();
+            comboBox1.Items.AddRange(ports);
+            if (comboBox1.Items.Count > 0) comboBox1.SelectedIndex = 0;
+
+            serialPort1.Encoding = Encoding.GetEncoding(1254);
+            serialPort1.DataReceived += new SerialDataReceivedEventHandler(SerialPort1_DataReceived);
+            progressBarHiz.Maximum = 150;
+            progressBarKilometre.Maximum = 999;
+            progressBarPil.Maximum = 100;
+            progressBarPilSicakligi.Maximum = 79;
+        }
+
+        private void SerialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            data = serialPort1.ReadLine();
+            this.Invoke(new EventHandler(displayData_event));
+        }
+
+        private void displayData_event(object sender, EventArgs e)
+        {
+            string temizVeri = data?.Trim();
+            if (string.IsNullOrEmpty(temizVeri)) return;
+
+            string[] parts = temizVeri.Split(new[] { ": " }, StringSplitOptions.None);
+            if (parts.Length != 2) return;
+
+            string etiket = parts[0];
+            string degerStr = parts[1];
+
+            if (Int16.TryParse(degerStr, out short value))
+            {
+                switch (etiket)
+                {
+                    case "Hiz":
+                        if (value >= 0 && value <= 120) progressBarHiz.Value = value;
+                        labelHiz.Text = "Hiz: " + degerStr;
+                        break;
+                    case "Kilometre":
+                        if (value >= 0 && value <= 1000) progressBarKilometre.Value = value;
+                        labelKilometre.Text = "Kilometre: " + degerStr;
+                        break;
+                    case "Pil":
+                        if (value >= 0 && value <= 100) progressBarPil.Value = value;
+                        labelPil.Text = "Pil: " + degerStr;
+                        break;
+                    case "Pil Sicakligi":
+                        if (value >= 20 && value <= 90) progressBarPilSicakligi.Value = value;
+                        labelPilSicakligi.Text = "Pil Sicakligi: " + degerStr;
+                        break;
+                }
+            }
+
+            dataQueue.Enqueue(temizVeri);
+            if (dataQueue.Count > 4) dataQueue.Dequeue();
+            textBox1.Text = string.Join(Environment.NewLine, dataQueue);
+        }
+
+        private void baslat_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboBox1.SelectedItem == null)
+                {
+                    MessageBox.Show("Lütfen bir port seçin.", "Hata");
+                    return;
+                }
+
+                serialPort1.PortName = comboBox1.SelectedItem.ToString();
+                serialPort1.BaudRate = 9600;
+                serialPort1.Open();
+                durdur.Enabled = true;
+                baslat.Enabled = false;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show($"Port '{serialPort1.PortName}' erişime kapalı. Yönetici olarak çalıştırın.", "İzin Hatası");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Hata");
+            }
+        }
+
+        private void durdur_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+                durdur.Enabled = false;
+                baslat.Enabled = true;
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (serialPort1.IsOpen) serialPort1.Close();
+        }
+
+        private void sifirla_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            dataQueue.Clear();
+            progressBarHiz.Value = 0;
+            progressBarKilometre.Value = 0;
+            progressBarPil.Value = 0;
+            progressBarPilSicakligi.Value = 0;
+            labelHiz.Text = "Hız: 0";
+            labelKilometre.Text = "Kilometre: 0";
+            labelPil.Text = "Pil: 0";
+            labelPilSicakligi.Text = "Pil Sıcaklığı: 0";
+        }
+
+        private void kaydet_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog sfd = new SaveFileDialog())
+            {
+                sfd.Filter = "Metin Dosyası (*.txt)|*.txt";
+                sfd.DefaultExt = "txt";
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllText(sfd.FileName, textBox1.Text, Encoding.GetEncoding(1254));
+                    MessageBox.Show("Veri başarıyla kaydedildi!", "Bilgi");
+                }
+            }
+        }
+    }
+}
